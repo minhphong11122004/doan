11de64a914167c19fd0a75eadf08c2d982b03255
minhphong11122004 <minhphong11122004@gmail.com>
2024-11-11 10:27:21 +0700

    sua dit page

--- src/page/Home.js
+++ src/page/Home.js
@@ -142,6 +142,77 @@ function Home() {
                     </div>
                 </div>
             )}
+            <div className="introduce">
+                <div className="introduce1">
+                    <h1 className="intro">
+                        50+ các loại vòng tay trang sức khác nhau
+                    </h1>
+                    <p className="in">
+                        Thiết kế của chúng tôi vẫn còn rất nhiều mẫu khác nhau, trong đó cũng có rất nhiều mẫu độc nhất và bán chạy trên thị trường
+                    </p>
+                </div>
+                <div className="introduce2">
+                    <img
+                        className={`minhhoa ${currentIndex === 0 ? "visible" : ""}`}
+                        src="https://cdn.pnj.io/images/detailed/138/gv0000w000690-vong-tay-vang-trang-y-18k-pnj-05.jpg"
+                        alt="Slideshow"
+                    />
+                    <img
+                        className={`minhhoa ${currentIndex === 1 ? "visible" : ""}`}
+                        src="https://th.bing.com/th/id/R.399bd796a096b1b4e83f6f618cda7138?rik=QVyM2V9KglG2rQ&riu=http%3a%2f%2fwww.exoticexcess.com%2fwp-content%2fuploads%2f2014%2f07%2fTiffany-Co.-Atlas-Hinged-Bangle-2.jpg&ehk=M3pxuDL4gpoag2T2YyMlZWG59SRFaoRQYUH038rCBDs%3d&risl=&pid=ImgRaw&r=0"
+                        alt="Slideshow"
+                    />
+                </div>
+
+            </div>
+            <div className="contact">
+                <div className="thongtin">
+                    <h1>
+                        Thong tin
+                    </h1>
+                    <p>
+                        Cơ sở : 331/7/52 Phan huy ích
+                    </p>
+                    <p>
+                        TPHCM: so dien thoai 0335552225
+                    </p>
+                    <p>
+                        Email: t2ngvn@gmail.com
+                    </p>
+
+                </div>
+                <div className="gioithieu">
+                    <h1>
+                        Menu
+                    </h1>
+                    <p>Sản phẩm</p>
+                    <p>Về chúng tôi</p>
+                    <p>Giấy phép và điều khoản</p>
+                </div>
+                <div className="taikhoan">
+                    <h1>
+                        Tài khoản
+                    </h1>
+                    <p>Tài khoản của bạn</p>
+                    <p>Kiểm tra tài khoản</p>
+                    <p>Giỏ hàng của bạn</p>
+                    <p>Đơn hàng cảu bạn</p>
+                </div>
+                <div className="lienhe">
+                    <h1>
+                        Liên hệ
+                    </h1>
+                    <p>Facebook</p>
+                    <p>Instagram</p>
+                    <p>Twitter</p>
+                </div>
+
+                <div className="phanhoi">
+                    <h1>phan hoi</h1>
+                    <input className='phanhoi1' type="text" placeholder="nhap email cua ban"></input>
+                    <button id="gui">Gửi</button>
+                </div>
+            </div>
         </div>
     );
 }
